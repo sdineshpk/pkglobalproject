68729f5a2e855473bbfb5c145e1787a89bdfbb83
Dinesh Kumar Sharma <sdinesh1@pkglobal.com>
2021-08-16 10:55:19 +0530

    auth related changes

--- apps/book-api/src/api-routes/app.routes.ts
+++ apps/book-api/src/api-routes/app.routes.ts
@@ -1,16 +1,48 @@
 import * as express from 'express';
 import * as bookServ from '../services/book.service';
 import * as reviewServ from '../services/reviews.service';
+import OktaJwtVerifier = require('@okta/jwt-verifier');
 export const routes = express.Router();
 
-routes.get("/books", bookServ.allBooks);
-routes.get("/books/:id", bookServ.getBook);
-routes.post("/books", bookServ.addBook);
-routes.put("/books/:id", bookServ.updateBook);
-routes.delete("/books/:id", bookServ.deleteBook);
+const oktaJwtVerifier = new OktaJwtVerifier({
+    issuer: 'https://dev-88273664.okta.com/oauth2/default' // required
+  });
 
-routes.get("/books/:id/reviews", reviewServ.allReviews);
-routes.get("/books/:id/reviews/:review_id", reviewServ.getReview);
-routes.post("/books/:id/reviews", reviewServ.addReview);
-routes.put("/books/:id/reviews/:review_id", reviewServ.updateReview);
-routes.delete("/books/:id/reviews/:review_id", reviewServ.deleteReview);
+
+
+// Only let the user access the route if they are authenticated.
+function ensureAuthenticated(req, res, next) {
+    const authHeader = req.headers.authorization || '';
+  const match = authHeader.match(/Bearer (.+)/);
+  // The expected audience passed to verifyAccessToken() is required, and can be either a string (direct match) or
+  // an array  of strings (the actual aud claim in the token must match one of the strings).
+  const expectedAudience = 'api://default';
+
+  if (!match) {
+    res.status(401);
+    return next('Unauthorized');
+  }
+
+  const accessToken = match[1];
+
+  return oktaJwtVerifier.verifyAccessToken(accessToken, expectedAudience)
+    .then((jwt) => {
+      req.jwt = jwt;
+      next();
+    })
+    .catch((err) => {
+      res.status(401).send(err.message);
+    });
+  }
+
+routes.get("/books",ensureAuthenticated, bookServ.allBooks);
+routes.get("/books/:id",ensureAuthenticated, bookServ.getBook);
+routes.post("/books",ensureAuthenticated, bookServ.addBook);
+routes.put("/books/:id",ensureAuthenticated, bookServ.updateBook);
+routes.delete("/books/:id",ensureAuthenticated, bookServ.deleteBook);
+
+routes.get("/books/:id/reviews",ensureAuthenticated, reviewServ.allReviews);
+routes.get("/books/:id/reviews/:review_id",ensureAuthenticated, reviewServ.getReview);
+routes.post("/books/:id/reviews",ensureAuthenticated, reviewServ.addReview);
+routes.put("/books/:id/reviews/:review_id",ensureAuthenticated, reviewServ.updateReview);
+routes.delete("/books/:id/reviews/:review_id",ensureAuthenticated, reviewServ.deleteReview);
--- apps/book-api/src/main.ts
+++ apps/book-api/src/main.ts
@@ -3,10 +3,35 @@ import * as path from 'path';
 import {routes} from './api-routes/app.routes';
 import * as OpenApiValidator from 'express-openapi-validator';
 import * as log4js from 'log4js';
+import session = require('express-session');
+import { ExpressOIDC } from '@okta/oidc-middleware';
+import * as cors from 'cors';
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
+
+// session support is required to use ExpressOIDC
+app.use(session({
+  secret: 'this should be secure',
+  resave: true,
+  saveUninitialized: false
+}));
+
+const oidc = new ExpressOIDC({
+  appBaseUrl:'http://localhost:3333',
+  issuer: 'https://dev-88273664.okta.com/oauth2/default',
+  client_id: '0oa1h0uljcJ5FhQMd5d7',
+  client_secret: 'FaZHabf6fK4v9JUdC8L944F3qHoCgBURGfzxwlOr',
+  redirect_uri: 'http://localhost:3333/authorization-code/callback',
+  scope: 'openid profile'
+});
+
+// ExpressOIDC attaches handlers for the /login and /authorization-code/callback routes
+app.use(oidc.router);
 
 //Logger
 const log = log4js.getLogger("app");
