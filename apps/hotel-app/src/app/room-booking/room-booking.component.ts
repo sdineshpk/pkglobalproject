29855c3a19553b766609486fd50f473cff03828c
Dinesh Kumar Sharma <sdinesh1@pkglobal.com>
2021-09-15 21:46:21 +0530

    added guest layer

--- apps/hotel-app/src/app/room-booking/room-booking.component.ts
+++ apps/hotel-app/src/app/room-booking/room-booking.component.ts
@@ -1,6 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, } from "@angular/forms"
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl} from "@angular/forms"
 import { ModalService } from '../_modal/modal.service';
+
+interface foodService {
+  name: string,
+  value: string
+}
+
+interface guests {
+  firstName: string,
+  lastName: string,
+  age:string,
+  gender:string,
+  email:string,
+  phone:string,
+  address:string
+}
 
 @Component({
   selector: 'pkglobal-room-booking',
@@ -10,10 +25,20 @@ import { ModalService } from '../_modal/modal.service';
 export class RoomBookingComponent {
 
   options="yourStay";
+  guestList:Array<guests>=[];
+  Data: Array<foodService> = [
+    { name: 'Breakfast', value: 'breakfast' },
+    { name: 'Lunch', value: 'lunch' },
+    { name: 'Dinner', value: 'Dinner' }
+  ];
 
   stayFormGroup: FormGroup=this.fb.group({
     checkDate: [null, Validators.required ],
-    checkOut: [null, Validators.required ]
+    checkOut: [null, Validators.required ],
+    roomType:[null, Validators.required],
+    person:[null, Validators.required],
+    checkArray: this.fb.array([], [Validators.required]),
+    pickup:[null, Validators.required],
  });
  guestFormGroup: FormGroup=this.fb.group({
   firstName: [null, Validators.required ],
@@ -35,6 +60,56 @@ export class RoomBookingComponent {
 
   closeModal(id: string) {
       this.modalService.close(id);
+  }
+
+  onCheckboxChange(e:any) {
+    const checkArray: FormArray = this.stayFormGroup.get('checkArray') as FormArray;
+
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i = 0;
+      checkArray.controls.forEach((item: any) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+  addGuest(){
+    this.guestList.push(this.guestFormGroup.value);
+    this.closeModal('custom-modal-1');
+  }
+
+  backToPrev(){
+    switch (this.options) {
+      case 'guests':
+        this.options='yourStay';
+        break;
+      case 'payment':
+        this.options='guests';
+        break;
+
+      default:
+        break;
+    }    
+  }
+
+  goToNext(){
+    switch (this.options) {
+      case 'guests':
+        this.options='payment';
+        break;
+      case 'yourStay':
+        this.options='guests';
+        break;
+
+      default:
+        break;
+    }
   }
 
 
